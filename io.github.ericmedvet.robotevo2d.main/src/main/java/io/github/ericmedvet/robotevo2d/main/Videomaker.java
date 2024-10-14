a2f3deeca04d99ade0ca6c79f0befdd89bcaf4d3
Francesco <rusinfrank98@gmail.com>
2024-10-14 23:10:37 +0200

    Extras.

--- io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/Videomaker.java
+++ io.github.ericmedvet.robotevo2d.main/src/main/java/io/github/ericmedvet/robotevo2d/main/Videomaker.java
@@ -21,6 +21,7 @@ package io.github.ericmedvet.robotevo2d.main;
 
 import io.github.ericmedvet.jgea.core.InvertibleMapper;
 import io.github.ericmedvet.jnb.core.NamedBuilder;
+import io.github.ericmedvet.jviz.core.drawer.Video;
 import io.github.ericmedvet.mrsim2d.core.agents.independentvoxel.AbstractIndependentVoxel;
 import io.github.ericmedvet.mrsim2d.core.engine.Engine;
 import io.github.ericmedvet.mrsim2d.core.tasks.AgentsObservation;
@@ -28,8 +29,11 @@ import io.github.ericmedvet.mrsim2d.core.tasks.AgentsOutcome;
 import io.github.ericmedvet.mrsim2d.core.tasks.locomotion.PrebuiltIndependentLocomotion;
 import io.github.ericmedvet.mrsim2d.viewer.TaskVideoBuilder;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -59,7 +63,7 @@ public class Videomaker {
                         "s.taskVideoBuilder(task = s.task.prebuiltIndependentLocomotion(terrain = s.terrain.holed(startW = 12; holeWs = [%d.05]); shape = s.a.vsr.shape.free(s = \"%s\")))"
                                 .formatted(hole, s));
         final BufferedReader reader = new BufferedReader(
-                new FileReader(path + "/Csv/coop-%dh-%s_best_fg.csv".formatted(hole, fitness)));
+                new FileReader(path + "/Csv/coop-%dh-%s-minih_best_fg.csv".formatted(hole, fitness)));
         List<Individual> individuals = new ArrayList<>();
         String line;
         while (Objects.nonNull(line = reader.readLine())) {
@@ -70,30 +74,11 @@ public class Videomaker {
                   Double.parseDouble(splitLine[2])));
         }
         reader.close();
-        Function<String, Function<Supplier<AbstractIndependentVoxel>, AgentsOutcome<AgentsObservation>>> PIL = s ->
-                (Function<Supplier<AbstractIndependentVoxel>, AgentsOutcome<AgentsObservation>>) nb.build(
-                        ("s.taskRunner(" +
-                                "task = s.task.prebuiltIndependentLocomotion(" +
-                                "terrain = s.terrain.holed(startW = 12; holeWs = [%d.05]); " +
-                                "shape = s.a.vsr.shape.free(s = \"%s\")" +
-                                ")" +
-                                ")")
-                                .formatted(hole, s));
         int index = -1;
-        List<Function<AgentsOutcome<AgentsObservation>, Double>> fitnessFunctions = List.of(
-                (Function<AgentsOutcome<AgentsObservation>, Double>) nb.build("sim.f.outcome.aaFinalMaxW()"),
-                (Function<AgentsOutcome<AgentsObservation>, Double>) nb.build("sim.f.outcome.aaFinalAvgW()"),
-                (Function<AgentsOutcome<AgentsObservation>, Double>) nb.build("sim.f.outcome.aaFinalMinW()")
-        );
         for (Individual i : individuals) {
-          ++index;
-          for (String s : List.of("ssssssssss", "ssssssss", "ssssss", "ssss")) {
-            AgentsOutcome<AgentsObservation> outcome = PIL.apply(s).apply(mapper.apply(i.genotype));
-            System.out.printf("%s, %s, %d; %f\n", s, fitnessFunctions.stream().map(f -> f.apply(outcome)).toList(), index, i.fitness);
-          }
-          /*Video video = videoBuilderBuilder.apply(i.s).apply(mapper.apply(i.genotype));
+          Video video = videoBuilderBuilder.apply(i.s).apply(mapper.apply(i.genotype));
           File file = new File(path + "/Videos/Hole%d_%s_%d_minih_vid%d.mp4".formatted(hole, fitness, i.s.length(), (++index) % 10));
-          Files.write(file.toPath(), video.data(), StandardOpenOption.WRITE, StandardOpenOption.CREATE);*/
+          Files.write(file.toPath(), video.data(), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
         }
       }
     }
